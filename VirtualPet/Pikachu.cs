8116478df0492d6467467f421ea5406091ae23cf
C <157691477+caellex@users.noreply.github.com>
2024-05-23 11:15:51 +0200

    Add project files.

---
+++ VirtualPet/Pikachu.cs
@@ -0,0 +1,63 @@
+﻿namespace VirtualPet;
+
+public class Pikachu
+{
+    private int _hunger;
+    private int _cleanliness;
+
+    public Pikachu(int hunger = 40, int cleanliness = 20)
+    {
+        _hunger = hunger;
+        _cleanliness = cleanliness;
+    }
+
+    public void ShowPikachu()
+    {
+        Console.Clear();
+        var originalColor = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("Pikachu\n");
+        Console.ForegroundColor = originalColor;
+        Console.WriteLine($"Hunger: {_hunger}");
+        Console.WriteLine($"Cleanliness: {_cleanliness}");
+    }
+
+    public void FeedPikachu()
+    {
+        Console.Clear();
+        Console.WriteLine("Pikachu has been fed!");
+        Console.WriteLine("Pikachu spilled food all over himself!");
+        _hunger += 20;
+        _cleanliness -= 10;
+        Thread.Sleep(1000);
+    }
+
+    public void CleanPikachu()
+    {
+        Console.Clear();
+        Console.WriteLine("Pikachu has been cleaned!");
+        Console.WriteLine("Pikachu is hungry!");
+        _hunger -= 10;
+        _cleanliness += 20;
+        Thread.Sleep(1000);
+    }
+
+    public void ReduceFoodAndOther()
+    {
+        _hunger -= 6;
+        _cleanliness -= 6;
+    }
+    public void PetPikachu()
+    {
+        Console.Clear();
+        Console.WriteLine("You pet Pikachu.");
+        Console.WriteLine("Pikachu smiles at you.");
+        Thread.Sleep(1000);
+    }
+
+    public static Pikachu LoadPikachu()
+    {
+        var pikachu = new Pikachu();
+        return pikachu;
+    }
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+
+namespace VirtualPet
+{
+    internal class Program
+    {
+        static Pikachu pikachu = Pikachu.LoadPikachu();
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Menu();
+                pikachu.ReduceFoodAndOther();
+                Game();
+                Thread.Sleep(2000);
+            }
+            
+        }
+
+        static void Game()
+        {
+            char menuChoice = Console.ReadKey().KeyChar;
+
+            switch (menuChoice)
+            {
+                case '1':
+                    pikachu.FeedPikachu();
+                    break;
+                case '2':
+                    pikachu.CleanPikachu();
+                    
+                    break;
+                case '3':
+                    pikachu.PetPikachu();
+                    
+                    break;
+                default:
+                    Main();
+                    break;
+            }
+        }
+
+        static void Menu()
+        {
+            pikachu.ShowPikachu();
+            Console.WriteLine();
+            Console.WriteLine("1) Feed Pikachu");
+            Console.WriteLine("2) Clean Pikachu");
+            Console.WriteLine("3) Pet Pikachu");
+            Console.WriteLine("4) Exit App");
+        }
+
+        /* Chættbått’n
+           Du skal lage en app hvor man kan lage sin egen chatbot - og deretter velge å snakke med den:)
+           Brukeren skal få prompt på chættbått navn, og deretter mulighet til å skrive inn forskjellige tilbakemeldinger 
+           svar som chættbått’n skal trekke et tilfeldig svar fra og svare brukeren med når den startes opp.
+           Bruk gjerne en meny for å gå inn i de forskjellige modusene: “mekke en chættbått” og “preke med en chættbått”
+           BONUS:
+           Mulighet til å lage flere chættbåtts, brukeren skal kunne se en liste av tilgjengelige chættbåtts og velge hvem h*n vil snakke med 
+           - her må det brukes List fra neste ukes pensum.
+        */
+    }
+}
